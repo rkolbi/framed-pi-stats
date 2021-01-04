c5468ca6950ace420abdb7e3ccfa721adc406333
RKolbi <19235632+rkolbi@users.noreply.github.com>
2021-01-04 16:57:24 +0900

    Create ht_logger.py

---
+++ root/ht_logger.py
@@ -0,0 +1,24 @@
+import os
+import time
+import Adafruit_DHT
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+try:
+    f = open('/var/www/html/enviro.csv', 'a+')
+    if os.stat('/var/www/html/enviro.csv').st_size == 0:
+            f.write('Date,Time,Temperature,Humidity\r\n')
+except:
+    pass
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+    if humidity is not None and temperature is not None:
+        f.write('{0} {1},{2:0.1f},{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity))
+        f.flush()
+    else:
+        print("Failed to retrieve data from humidity sensor")
+
+    time.sleep(60)
