5cd5418ead13c0ad40402a107f30d7d9110d6286
RKolbi <19235632+rkolbi@users.noreply.github.com>
2021-11-27 04:55:56 +0900

    Add files via upload

---
+++ web-root/mix-do.php
@@ -0,0 +1,13 @@
+<?php
+
+if (($_POST['mix']) == 'mix') {
+
+   shell_exec("echo 'G28 F600 G0 Z5 F70 G0 Z1.0 F600 G0 Z15 F70 G0 Z1.0 F600 G0 Z10 F70 G0 Z1.0 F600 G0 Z5 F70 G0 Z1.0 F600 G0 Z15 F70 G0 Z1.0 F600 G0 Z10 F70 G0 Z1.0 F600 G0 Z5 F70 G0 Z1.0 F600 G0 Z15 F70 G0 Z1.0 F600 G0 Z10 F70 G0 Z1.0 F600 G0 Z5 F70 G0 Z1.0 F600 G0 Z15 F70 G0 Z1.0 F600 G0 Z10 F70 G0 Z1.0 F600 G0 Z5 F70' > /dev/serial0");
+   flush();
+
+} 
+
+  header('Location: rpi.php');
+
+
+?>
